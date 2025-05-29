dc962676e9788489a3bba5878284ada15a884bc0
Hamza Sarwar <hamzasarwarofficial99@gmail.com>
2025-05-29 15:20:54 +0500

    - completed animation setup

--- src/navigations/AppNavigation.tsx
+++ src/navigations/AppNavigation.tsx
@@ -1,14 +1,21 @@
-import {createNativeStackNavigator} from 'react-native-screens/native-stack';
 import OnBoarding from '../screens/onboarding/onBoarding.tsx';
 import {SCREEN_NAMES} from '../constants/screenNamesConstants.ts';
 import HomeScreen from '../screens/home/homeScreen.tsx';
 import {useEffect, useState} from 'react';
 import {checkFirstLaunch} from '../services/firstLaunchService.ts';
 import {ActivityIndicator, StyleSheet, View} from 'react-native';
+import {
+  createStackNavigator,
+  StackNavigationOptions,
+} from '@react-navigation/stack';
 
-const Stack = createNativeStackNavigator();
+const RootStack = createStackNavigator();
 
 const AppNavigations = () => {
+  const ROOT_STACK_OPTIONS: StackNavigationOptions = {
+    headerShown: false,
+    gestureEnabled: false,
+  };
   const [initialRoute, setInitialRoute] = useState<string | null>(null);
 
   useEffect(() => {
@@ -31,17 +38,26 @@ const AppNavigations = () => {
   }
 
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName={initialRoute}>
-      <Stack.Screen
+    <RootStack.Navigator
+      screenOptions={ROOT_STACK_OPTIONS}
+      initialRouteName={initialRoute}>
+      <RootStack.Screen
         name={SCREEN_NAMES.ONBOARDING_SCREEN}
         component={OnBoarding}
       />
-      <Stack.Screen name={SCREEN_NAMES.HOME_SCREEN} component={HomeScreen} />
-    </Stack.Navigator>
+      <RootStack.Screen
+        name={SCREEN_NAMES.HOME_SCREEN}
+        component={HomeScreen}
+      />
+    </RootStack.Navigator>
   );
 };
 
 export default AppNavigations;
 const styles = StyleSheet.create({
-  loadingContainer: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
